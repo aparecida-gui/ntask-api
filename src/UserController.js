cad65adad53ac938f0e7801e407d2f04f434ad53
Suelen Guimarães <suelen.ads.guimaraes@gmail.com>
2020-01-21 16:45:15 -0300

    Refactore of route authenticatiom and register of user.

--- src/UserController.js
+++ src/UserController.js
@@ -1,21 +1,22 @@
 import User from './user';
-import bcrypt from 'bcrypt';
-import { bcryptPassword, token } from './help';
+import { bcryptPassword, bcryptCompare, token } from './help';
 
 const checksUser = async (req, res) => {
+  const { email } = req.body;
   req.body.password = await bcryptPassword(req.body.password);
 
   try {
-    const user = User.create(req.body);
-    const tokenGeneral = await token({ id: user.id });
-    console.log('tokenGeneral: ', tokenGeneral);
-    user
-      .then(userData => {
-        res.status(201).json({ message: 'Created User.', userData, tokenGeneral });
-      })
-      .catch(() => res.status(403).json({ error: 'user exist' }));
+    if (!(await User.findOne({ email }))) {
+      const user = await User.create(req.body);
+      const tokenGeneral = await token({ id: user.id });
+      await res
+        .status(201)
+        .json({ message: 'Created User.', user, tokenGeneral });
+    } else {
+      await res.status(400).json({ errorMessage: 'user exist.' });
+    }
   } catch (error) {
-    res.status(404).json({ error: error });
+    await res.status(500).json({ errorMessage: error });
   }
 };
 
@@ -24,7 +25,7 @@ const authenticationUser = async (req, res) => {
   const user = await User.findOne({ email }).select('+password');
 
   if (await user) {
-    if (await bcrypt.compare(password, user.password)) {
+    if (await bcryptCompare(password, user.password)) {
       const tokenGeneral = await token({ id: user.id });
       return res
         .status(200)
