ebbca09a3cb218ed356126817007a1dff430eda2
Suelen Guimarães <suelen.ads.guimaraes@gmail.com>
2020-01-21 15:57:55 -0300

    Add method de compare of bcrypt.

--- src/help.js
+++ src/help.js
@@ -1,10 +1,14 @@
 import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import jsonwebtoken from 'jsonwebtoken';
 
 export const bcryptPassword = async password => {
   return await bcrypt.hash(password, 10);
 };
 
+export const bcryptCompare = async (dataRequest, data) => {
+  return await bcrypt.compare(dataRequest, data);
+};
+
 export const token = async ({ data }) => {
-  return await jwt.sign({ data }, 'sdsdsdsdsds', { expiresIn: 86400 });
+  return await jsonwebtoken.sign({ data }, 'sdsdsdsdsds', { expiresIn: 86400 });
 };
