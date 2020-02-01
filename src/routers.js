8c2f79cb751d993d78bad49bb74d77f045858388
Suele Guimarães <suelen.ads.guimaraes@gmail.com>
2020-01-31 22:24:29 -0300

    Add module sendgrid.

--- src/routers.js
+++ src/routers.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import TaskController from './TaskController';
 import UserController from './UserController';
+import TaskController from './TaskController';
+import SendEmailController from './SendEmailController';
 import verifyToken from './verifyAuth';
 const router = express.Router();
 
 router.post('/user/register', UserController.checksUser);
 router.post('/user/authenticate', UserController.authenticationUser);
+router.post('/recover', SendEmailController.sendEmail);
 router.use(verifyToken);
 router.get('/', TaskController.showTask);
 router.post('/', TaskController.createTask);
